4afc784470a0bb6f5097355a937471c6ac14f61e
vietdh <vietdanghuy.95@gmail.com>
2024-03-03 23:47:32 +0700

    Show basic router pages

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="home-page">
+      <div class="column">
+        <h1>Welcome to the Frontend Quiz!</h1>
+        <p>Pick a subject to get started down below.</p>
+      </div>
+      <div class="column">
+        <ul class="subject-list">
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+          <li>Accessibility</li>
+        </ul>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage'
+  }
+  </script>
+  
+  <style scoped>
+  .home-page {
+    display: flex;
+    justify-content: space-between;
+  }
+  
+  .column {
+    width: 45%; /* Adjust as needed */
+  }
+  
+  .subject-list {
+    list-style-type: none;
+    padding: 0;
+  }
+  
+  .subject-list li {
+    margin-bottom: 20px; /* Adjust as needed */
+  }
+  </style>
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import {createRouter, createWebHistory} from 'vue-router'
+import HomePage from '@/pages/HomePage.vue'
+
+const routes = [
+  // other routes...
+  {
+    path: '/',
+    name: 'HomePage',
+    component: HomePage
+  }
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+export default router;
