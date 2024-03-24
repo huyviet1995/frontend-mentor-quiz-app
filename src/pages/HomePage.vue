b75a77baddf34b869c8458647bce80ca0ea6918d
vietdh <vietdanghuy.95@gmail.com>
2024-03-24 15:27:52 +0700

    Add icons, fonts and variables

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="container flex flex-row justify-between max-w-5xl mx-auto">
         <div class="">
-            <h1 class="mb-5">Frontend Quiz!</h1>
+            <h1 class="mb-5">
+                <span>Welcome to the</span>
+                <br/>
+                Frontend Quiz!
+            </h1>
             <p>Pick a subject to get started</p>
         </div>
         <div class="">
@@ -21,9 +25,8 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 h1 {
-    font-family: "Rubik";
     font-style: normal;
     font-weight: 500;
     font-size: 64px;
@@ -32,11 +35,20 @@ h1 {
     flex: none;
     order: 1;
     flex-grow: 0;
+    span {
+        font-style: normal;
+        font-weight: 300;
+        font-size: 64px;
+        line-height: 100%;
+        color: var(--dark-navy);
+        flex: none;
+        order: 0;
+        flex-grow: 0;
+    }
 }
 
 p {
     /* Pick a subject to get started. */
-    font-family: "Rubik";
     font-style: italic;
     font-weight: 400;
     font-size: 20px;
